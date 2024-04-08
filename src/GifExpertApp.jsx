bf57ae24c377b38b704d412b726ad9d4671efe2e
NickEsColR <nicoesol0818@gmail.com>
2024-04-08 13:16:36 -0500

    sesion 76

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(["One Punch"]);
     return (
         <>
+            {/* titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* input */}
+
+            {/* Listado de gifs */}
+            <ol>
+                {
+                    categories.map(category => (
+                        <li key={category}>{category}</li>
+                    ))
+                }
+            </ol>
+                {/* GifItem */}
         </>
     );
 };
