9ba18dff555cc6d9524af9f761defb9a61217104
NickEsColR <nicoesol0818@gmail.com>
2024-04-08 17:41:17 -0500

    styles: add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 import { GifExpertApp } from "./GifExpertApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
