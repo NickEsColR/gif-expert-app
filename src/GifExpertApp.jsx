36d4ca861cb4736f94ae4604354746d4ea489f89
NickEsColR <nicoesol0818@gmail.com>
2024-04-08 13:24:54 -0500

    sesion 77

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,8 +2,15 @@ import { useState } from "react";
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch"]);
+
+    const onAddCategory = () => {
+        // setCategories([...categories, "HunterXHunter"]); //opcion 1
+        //setCategories(cats => [...cats, "HunterXHunter"]); //opcion 2
+        setCategories(cats => cats.includes("HunterXHunter") ? cats : [...cats, "HunterXHunter"]); //opcion 3 filtrando
+    }
     return (
         <>
+            <button onClick={onAddCategory}>Agregar</button>
             {/* titulo */}
             <h1>GifExpertApp</h1>
 
