6ae82fe07767bea66855ff25840424412fc7817c
NickEsColR <nicoesol0818@gmail.com>
2024-04-08 15:14:59 -0500

    sesion 86

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+    const [gifs, setGifs] = useState([]);
+
+    const initializeGifs = async () => {
+        const gifs = await getGifs(category);
+        setGifs(gifs);
+    };
+
     useEffect(() => {
-        getGifs(category);
+        initializeGifs();
     }, []);
 
     return (
         <>
             <h3>{category}</h3>
+            {gifs.map(({ id, title, url }) => (
+                <div key={id} className="card">
+                    <img src={url} alt={title} />
+                    <p>{title}</p>
+                </div>
+            ))}
         </>
     );
 };
